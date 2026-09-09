5c75d1380aeacea125330aa0752778a751d3c3d0
okancimen <okan@cimen.net>
2026-09-09 14:22:32 +0400

    Revert "perf: move Clarity and GA scripts to lazyOnload to reduce TBT"
    This reverts commit be727055233796236bddcebfa7b55227f2f20e86.

--- components/ConditionalAnalytics.tsx
+++ components/ConditionalAnalytics.tsx
@@ -16,8 +16,8 @@ export default function ConditionalAnalytics() {
 
   return (
     <>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-F60J3BHJDE" strategy="lazyOnload" />
-      <Script id="google-analytics" strategy="lazyOnload">{`
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-F60J3BHJDE" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">{`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
